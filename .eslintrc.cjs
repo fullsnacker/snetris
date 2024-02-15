cabdd58114819fbd1b1df29a76dd789de2970b3b
fullsnacker <fullsnacker@gmail.com>
2024-02-15 16:37:06 -0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'eslint-config-prettier'
+	],
+	overrides: [
+		{
+			env: {
+				node: true
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script'
+			}
+		}
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	plugins: ['react'],
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+		'react/react-in-jsx-scope': 0,
+		'no-unused-vars': 0,
+		'react/prop-types': 0
+	},
+	settings: {
+		react: {
+			version: 'detect'
+		}
+	}
+};
